d0c3f1fa6b44b92b30ed55579da4a5854b2f5fd1
Scott Clay <sclay@n.io>
2017-08-10 16:52:01 -0600

    Format Better

--- PID_block.py
+++ PID_block.py
@@ -10,12 +10,10 @@ class PID(Block):
     Kp = FloatProperty(title="P", default=2.0)
     Ki = FloatProperty(title="I", default=0)
     Kd = FloatProperty(title="D", default=1.0)
-    # Derivator = FloatProperty(title="Derivator", default=0)
-    # Integrator = FloatProperty(title="Integrator", default=0)
     Integrator_max = FloatProperty(title="Integrator_max", default=500, visible=False)
     Integrator_min = FloatProperty(title="Integrator_min", default=-500, visible=False)
-    current_value = Property(title='Current Value', default='')
-    set_point = FloatProperty(title="Set Point", default=0)
+    current_value = Property(title='Current Value', default=0)
+    set_point = FloatProperty(title='Set Point', default=0)
 
 
     def __init__(self):
@@ -35,24 +33,24 @@ class PID(Block):
         self.notify_signals(new_signals)
 
 
-    def _update(self,current_value):
+    def _update(self, current_value):
         """
         Calculate PID output value for given reference input and feedback
         """
 
         self.error = self.set_point() - current_value
-        self.P_value = self.Kp * self.error
-        self.D_value = self.Kd * ( self.error - self.Derivator)
-        self.Derivator = self.error
+        self.P_value = self.Kp() * self.error
+        self.D_value = self.Kd() * (self.error - self.Derivator)
 
+        self.Derivator = self.error
         self.Integrator = self.Integrator + self.error
 
-        if self.Integrator > self.Integrator_max:
-            self.Integrator = self.Integrator_max
-        elif self.Integrator < self.Integrator_min:
-            self.Integrator = self.Integrator_min
+        if self.Integrator > self.Integrator_max():
+            self.Integrator = self.Integrator_max()
+        elif self.Integrator < self.Integrator_min():
+            self.Integrator = self.Integrator_min()
 
-        self.I_value = self.Integrator * self.Ki
+        self.I_value = self.Integrator * self.Ki()
 
         PID = self.P_value + self.I_value + self.D_value
 
--- tests/test_PID_block.py
+++ tests/test_PID_block.py
@@ -1,6 +1,7 @@
 from nio.block.terminals import DEFAULT_TERMINAL
 from nio.signal.base import Signal
 from nio.testing.block_test_case import NIOBlockTestCase
+from unittest.mock import MagicMock, ANY
 from PID_block import PID
 
 
@@ -8,18 +9,14 @@ class TestPID(NIOBlockTestCase):
 
     def test_process_signals(self):
         """Signals pass through block unmodified."""
+        value = {'clear' : ANY}
+        
         blk = PID()
-        self.configure_block(blk, {
-                                   'kp' : "{{ $kp }}",
-                                   'ki' : "{{ $ki }}",
-                                   'kd' : "{{ $kd }}",
-                                   'setpoint' : "{{ $setpoint }}",
-                                   'current_value' : "{{ $current_value }}"
-        })
+        self.configure_block(blk, {})
         blk.start()
-        blk.process_signals([Signal()])
+        blk.process_signals([Signal(value)])
         blk.stop()
         self.assert_num_signals_notified(1)
         self.assertDictEqual(
-            {"PID": 2},
+            {'value' : ANY},
             self.last_notified[DEFAULT_TERMINAL][0].to_dict())
