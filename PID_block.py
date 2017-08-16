f30b6f643949496ef6a6e9db35357854afca4057
Scott Clay <sclay@n.io>
2017-08-16 11:51:59 -0600

    Test confirms change in signal with time

--- PID_block.py
+++ PID_block.py
@@ -37,29 +37,34 @@ class PID(Block):
     def process_signals(self, signals):
         new_signals = []
         for signal in signals:
+
             dt = (datetime.datetime.utcnow() - self.last_time).total_seconds()
+            print('dt {}'.format(dt))
             self.logger.debug('dt {}'.format(dt))
 
-            """Calculation for Proportional Gain"""
+            #Calculation for Proportional Gain, Error==SP-ProcessVariable
             self.error = self.process_config().set_point(signal) - \
                          self.process_config().current_value(signal)
             self.logger.debug('error {}'.format(self.error))
+            print('error {}'.format(self.error))
             self.P_value = self.gain_config().Kp(signal) * self.error
 
-            """Calculation for Derivative Gain, Derivator==Previous Error"""
+            #Calculation for Derivative Gain, Derivator==Previous Error
             self.D_value = \
                     self.gain_config().Kd(signal) * \
                     (self.error - self.Derivator) / dt
             self.Derivator = self.error
             self.logger.debug('Derivator {}'.format(self.Derivator))
-
-            """Calcualation for Integral Gain, Integrator==Sum of all Errors"""
+            print('Derivator {}'.format(self.Derivator))
+            print('D {}'.format(self.D_value))
+            #Calcualation for Integral Gain, Integrator==Sum of all Errors
             self.Integrator = self.Integrator + self.error * dt
             self.logger.debug('Integrator {}'.format(self.Integrator))
+            print('Integrator {}'.format(self.Integrator))
             self.last_time = datetime.datetime.utcnow()
             self.I_value = self.Integrator * self.gain_config().Ki(signal)
-
-            """Final Math Addition of all terms"""
+            print('I {}'.format(self.I_value))
+            #Final Math Addition of all terms
             PID = self.P_value + self.I_value + self.D_value
             new_signals.append(Signal({'value' : PID}))
         self.notify_signals(new_signals)
--- tests/test_PID_block.py
+++ tests/test_PID_block.py
@@ -24,8 +24,11 @@ class TestPID(NIOBlockTestCase):
     def test_config_signal(self):
 
         blk = PID()
-        # blk.method = MagicMock()
-        # blk.method.assert_called_with()
+        pv = 5
+        setp = 10
+        kp = 1
+        ki = 1
+        kd = 1
         self.configure_block(blk, {'process_config' :
                                         {'set_point' : '{{$sp}}',
                                          'current_value' : '{{$value}}'},
@@ -35,15 +38,24 @@ class TestPID(NIOBlockTestCase):
                                          'Kd' : '{{$Kd}}'
         } })
         blk.start()
-        blk.process_signals([Signal({'value' : 5,
-                                     'sp' : 10,
-                                     'Kp' : 1,
-                                     'Ki' : 0,
-                                     'Kd' : 0
-        })])
+        blk.process_signals([Signal({'value' : pv,
+                                     'sp' : setp,
+                                     'Kp' : kp,
+                                     'Ki' : ki,
+                                     'Kd' : kd
+        }),Signal({'value' : pv,
+                                     'sp' : setp,
+                                     'Kp' : kp,
+                                     'Ki' : ki,
+                                     'Kd' : kd
+        })] )
         # block.process_signals([Signal({'test_count': 1})])
         blk.stop()
-        self.assert_num_signals_notified(1)
-        self.assertDictEqual(
-            {'value' : 5},
-            self.last_notified[DEFAULT_TERMINAL][0].to_dict())
+        self.assert_num_signals_notified(2)
+        print([n.to_dict() for n in self.last_notified[DEFAULT_TERMINAL]])
+        self.assertFalse(
+            self.last_notified[DEFAULT_TERMINAL][0].to_dict()==\
+            self.last_notified[DEFAULT_TERMINAL][1].to_dict())
+        # self.assertDictEqual(
+            # {'value' : ANY},
+            # self.last_notified[DEFAULT_TERMINAL][0].to_dict())
