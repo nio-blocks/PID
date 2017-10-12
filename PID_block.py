3357285588cdf28aca2370dfc04f3dfb13d577a5
neutralioJenkins <jenkins@n.io>
2017-10-12 16:02:21 -0600

    Bump version to v0.1.1

--- PID_block.py
+++ PID_block.py
@@ -23,7 +23,7 @@ class GainConfig(PropertyHolder):
 
 class PID(EnrichSignals, Block):
 
-    version = VersionProperty("0.1.0")
+    version = VersionProperty("0.1.1")
     process_config = ObjectProperty(
         ProcessConfig, title="Process Variable Setup", default=ProcessConfig())
     gain_config = ObjectProperty(
